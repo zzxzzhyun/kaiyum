6ac0fbfb40315f0b0e1f9cba27cffdda1d77123d
dev-jaemin <jay.kimjaemin@gmail.com>
2022-07-10 18:20:43 +0900

    solve conflict with main

--- app/src/main/java/com/example/kaiyum/ui/campus/RetrofitService.java
+++ app/src/main/java/com/example/kaiyum/ui/campus/RetrofitService.java
@@ -5,6 +5,7 @@ import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
 import retrofit2.http.Path;
